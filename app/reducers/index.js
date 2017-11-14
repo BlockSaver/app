719148051945ff8226a3c9cdc6041b60fa21a92c
Marin Petrunić <marin.petrunic@gmail.com>
2017-11-14 23:29:18 +0100

    cleaning up

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,9 @@
 // @flow
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
-import counter from './counter';
-import wallet from "./wallet";
+import wallet from './wallet';
 
 const rootReducer = combineReducers({
-  counter,
   router,
   wallet,
 });
--- app/routes.js
+++ app/routes.js
@@ -7,14 +7,12 @@ import App from './containers/App';
 import HomePage from './containers/HomePage';
 import SavingsPage from './containers/SavingsPage';
 import SettingsPage from './containers/SettingsPage';
-import CounterPage from './containers/CounterPage';
 import SavingsForm from './containers/AddNewSaving';
 
 export default () => (
   <StripeProvider apiKey="pk_test_iV3fX2RwYAeC2L2DcmIEvBTL">
     <App>
       <Switch>
-        <Route path="/counter" component={CounterPage} />
         <Route exact path="/savings/new" component={SavingsForm} />
         <Route path="/savings" component={SavingsPage} />
         <Route path="/settings" component={SettingsPage} />
